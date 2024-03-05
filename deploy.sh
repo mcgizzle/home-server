64ddd95da8bbe09a5cd0b80a0c86adc44259e97c
mcgizzle <sean@permutive.com>
2024-03-05 20:25:42 +0000

    Fixes for deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,22 @@
 
 # List of directories with docker-compose.yml files
 directories=(
-  '/apps/dashboard'
+  'apps/dns'
+  'apps/reverse-proxy'
+  'apps/pvr'
+  'apps/vpn'
+  'apps/monitoring'
+  'apps/qbit'
+  'apps/media/plex'
+  'apps/portainer'
+  'apps/dashboard'
+  'apps/watchtower'
+  #'apps/tailscale'
 )
 
 for dir in "${directories[@]}"; do
   echo "Starting server in $dir"
-  (cd "$dir" && docker-compose up -d)
+  (cd "$dir" && docker compose up -d)
 done
 
 echo "All projects started."
