4b61720ad7c1f398486629faa801db92ef772ec6
mcgizzle <sean@permutive.com>
2024-08-30 20:59:37 +0100

    Separates out apps & services by function

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # home-server
 
-Running on a yummy pi
+The repo is split into `apps` and `infra` directories. The `apps` directory contains all the applications that run on the server. The `infra` directory contains all the infrastructure code that provisions the server.
+
+Within `apps`, there are sets of services for two hosts:
+1. `primary` - The primary host that runs most services (e.g. plex, *arr, etc.)
+2. `network` - The network host that runs services that are critical for the server to function. (e.g. DNS, P2P VPN)
+
+## Deploy
+```shell
+./deploy.sh <primary|network> [--pull-only] [--restart]
+```
--- deploy.sh
+++ deploy.sh
@@ -1,47 +1,59 @@
 #!/bin/bash
 
+set -euo pipefail
+
 SCRIPT_DIR=$(dirname "$0")
 source "${SCRIPT_DIR}/.env"
 
 pull_only=false
 restart=false
 
-while [[ "$#" -gt 0 ]]; do
-  case $1 in
+command=$1
+if [ "$command" = "primary" ]; then
+  apps=$(ls "$SCRIPT_DIR"/apps/primary)
+elif [ "$command" = "network" ]; then
+  apps=$(ls "$SCRIPT_DIR"/apps/network)
+else
+  echo "Unknown command: $command"
+  usage
+  exit 1
+fi
+
+while [[ "$#" -gt 1 ]]; do
+  case $2 in
     -r|--restart) restart=true ;;
-    --pull-only) pull_only=true ;;
-    *) echo "Unknown option: $1"; exit 1 ;;
+    -p|--pull-only) pull_only=true ;;
+    *) echo "Unknown option: $2$"; usage; exit 1 ;;
   esac
   shift
 done
 
-directories=(
-  'apps/vpn'
-  'apps/qbit'
-  'apps/reverse-proxy'
-  'apps/pvr'
-  'apps/cloudflared'
-  'apps/media/plex'
-  'apps/monitoring'
-#  'apps/portainer'
-#  'apps/dns'
-#  'apps/tailscale'
-)
+function usage () {
+  echo "Usage: deploy.sh <primary|network> [options]"
+  echo "Options:"
+  echo "  -r, --restart  Restart the app"
+  echo "  -p, --pull-only  Pull images only"
+}
 
-for dir in "${directories[@]}"; do
-  echo "Processing $dir"
-  (cd "$dir" && if [ "$pull_only" = true ]; then
-    echo "Pulling images in $dir"
-    docker compose pull
-  elif [ "$restart" = true ]; then
-    echo "Restarting app in $dir"
-    docker compose down
-    docker compose up -d --force-recreate
-  else
-    echo "Starting server in $dir"
-    docker compose up -d
-  fi)
-done
+function deploy () {
+  apps=$1
+  for dir in "${apps[@]}"; do
+    echo "Processing $dir"
+    (cd "$dir" && if [ "$pull_only" = true ]; then
+      echo "Pulling images in $dir"
+      docker compose pull
+    elif [ "$restart" = true ]; then
+      echo "Restarting app in $dir"
+      docker compose down
+      docker compose up -d --force-recreate
+    else
+      echo "Starting server in $dir"
+      docker compose up -d
+    fi)
+  done
+}
+
+deploy "$apps"
 
 if [ "$pull_only" = true ]; then
   echo "All images pulled."
