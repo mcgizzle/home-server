aad9f3412e1dad4b856279cb716023de712b73bd
mcgizzle <sean@permutive.com>
2025-05-05 15:13:54 +0100

    feat(server): updates to run in Mo's Manor

--- apps/cloud/nfl/sync-db-local.sh
+++ apps/cloud/nfl/sync-db-local.sh
@@ -1,1 +1,2 @@
-scp root@192.168.1.99:/root/code/home-server/apps/cloud/nfl/data/results.db data/results.db
+source ../../../env.sh
+scp root@"${PRIMARY_IP}":/root/code/home-server/apps/cloud/nfl/data/results.db data/results.db
